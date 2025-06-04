953f2eade5e8ff2722404a896b2cc820f04b7d5a
karankoder <karankr.das.cse22@iitbhu.ac.in>
2025-06-05 01:26:10 +0530

    init project

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { config } from 'dotenv';
+import { errorMiddleware } from './middlewares/error.js';
+import cors from 'cors';
+
+export const app = express();
+export const backendUrl =
+  process.env.NODE_ENV === 'development'
+    ? process.env.LOCAL_BACKEND_URL
+    : process.env.BACKEND_URL;
+
+
+config({
+  path: './.env',
+});
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Server is working');
+});
+
+app.get('/failure', (req, res) => {
+  res.send('Failed to Login');
+});
+
+app.use(errorMiddleware);
---
+++ config/database.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+export const connectDB = () => {
+  const mongo_uri =
+    process.env.NODE_ENV === 'development'
+      ? process.env.LOCAL_MONGO_URI
+      : process.env.MONGO_URI;
+  mongoose
+    .connect(mongo_uri, {
+      dbName: 'StudyTrack',
+    })
+    .then((c) => {
+      console.log(`Database Connected with ${c.connection.host}`);
+    })
+    .catch((err) => {
+      console.log('Database Connection failed');
+      console.log(err);
+    });
+};
